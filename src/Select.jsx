0a14a5dc1c12a0fe612906de4ae96bfdd33d0b3e
Brandon Might <might.brandon@gmail.com>
2019-08-22 12:36:29 -0400

    Select set value if undefined or inject value if not in options

--- src/Select.jsx
+++ src/Select.jsx
@@ -1,16 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from 'reactstrap'
 import FieldGroup from './FieldGroup';
 
 const Options = (props) => {
-  if (props.children) {
-    return props.children
+  const { options, children, insertOption } = props
+  if (children) {
+    return (
+      <>
+        {children}
+        {insertOption
+          ? <option value={insertOption} disabled>{insertOption}</option>
+          : null
+        }
+      </>
+    ) 
   } else if (props.options) {
-    return props.options.map((opt, index) => {
+    const opts = props.options.map((opt, index) => {
       return (<option {...opt} key={index}>{opt.text}</option>)
     })
+    return (
+      <>
+        {opts}
+        {insertOption
+          ? <option value={insertOption} disabled>{insertOption}</option>
+          : null
+        }
+      </>
+    ) 
   } else {
     return null
+  }
+}
+
+const getFirstOptionValue = (options, children) => {
+  if (children) {
+    const child = React.Children.toArray(children)[0]
+    return child.props.value
+  } else if (options) {
+    const option = options[0]
+    return option.value
+  } else {
+    return ''
+  }
+}
+
+const checkOptionAvailable = (value, options, children) => {
+  if (children) {
+    const child = React.Children.toArray(children).find(c => {
+      if(c.props) {
+        return c.props.value == value
+      } 
+      return false
+    })
+    return !!child
+  } else if (options) {
+    const option = options.find(o => o.value === value)
+    return !!option
+  } else {
+    return false
   }
 }
 
@@ -18,14 +65,33 @@ const Select = (props) => {
   return (
     <FieldGroup 
       {...props}
-      render={(fieldProps) => (
-        <Input
-          {...fieldProps}
-          type='select'
-        >
-          <Options options={fieldProps.options} children={fieldProps.children} />
-        </Input>
-      )}
+      render={(fieldProps) => { 
+        const [ insertOption , setInsertOption ] = useState()
+        useEffect(() => {
+          if (fieldProps.value === undefined) {
+            const firstOptionValue = getFirstOptionValue(fieldProps.options, fieldProps.children)
+            fieldProps.formik.form.setFieldValue(fieldProps.name, firstOptionValue)
+          } else {
+            const optionExists = checkOptionAvailable(fieldProps.value, fieldProps.options, fieldProps.children)
+            if (!optionExists) {
+              setInsertOption(fieldProps.value) 
+            }
+          }
+        }, [])
+
+        return (
+          <Input
+            {...fieldProps}
+            type='select'
+          >
+            <Options
+              options={fieldProps.options}
+              children={fieldProps.children}
+              insertOption={insertOption}
+            />
+          </Input>
+        )
+      }}
     />
   )
 }
